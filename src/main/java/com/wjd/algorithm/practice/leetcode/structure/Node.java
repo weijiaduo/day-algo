c7ca04a21b9853bcb503226b6a88a8bde9be59ca
weijiaduo <648620282@qq.com>
2022-06-26 12:39:31 +0800

    更新数据结构

--- src/main/java/com/wjd/algorithm/practice/leetcode/structure/Node.java
+++ src/main/java/com/wjd/algorithm/practice/leetcode/structure/Node.java
@@ -1,21 +1,40 @@
 package com.wjd.algorithm.practice.leetcode.structure;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
+/**
+ * 通用节点（链表、树、图）
+ *
+ * @author weijiaduo
+ * @since 2022-06-26
+ */
 public class Node {
 
     public int val;
+    /**
+     * 链表节点
+     */
     public Node next;
+    /**
+     * 随机指针
+     */
+    public Node random;
+    /**
+     * 树节点
+     */
     public Node left;
     public Node right;
+    /**
+     * 图节点
+     */
+    public List<Node> neighbors;
 
-    public Node() {}
+    public Node() {
+    }
 
     public Node(int val) {
         this.val = val;
+        neighbors = new ArrayList<>();
     }
 
     public Node(int val, Node next) {
@@ -33,7 +52,7 @@ public class Node {
         }
         return head;
     }
-    
+
     public static Node buildTree(Integer[] values) {
         if (values == null || values.length == 0) {
             return null;
@@ -73,10 +92,51 @@ public class Node {
     }
 
     /**
+     * 构建图
+     */
+    public static Node buildGraph(int[][] adjList) {
+        Node node = null;
+        Map<Integer, Node> nodes = new HashMap<>(adjList.length);
+        for (int[] edge : adjList) {
+            node = nodes.get(edge[0]);
+            if (node == null) {
+                node = new Node(edge[0]);
+                nodes.put(edge[0], node);
+            }
+            Node node2 = nodes.get(edge[1]);
+            if (node2 == null) {
+                node2 = new Node(edge[1]);
+                nodes.put(edge[1], node2);
+            }
+            node.neighbors.add(node2);
+        }
+        return node;
+    }
+
+    /**
+     * 按链表遍历
+     */
+    public String listString() {
+        Node p = this;
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        while (p != null) {
+            sb.append(p.val).append(',');
+            p = p.next;
+            if (p == this) {
+                break;
+            }
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append(']');
+        return sb.toString();
+    }
+
+    /**
      * 层次遍历
      */
-    public static List<Integer> bfs(Node tree){
-        if (tree == null){
+    public static List<Integer> bfsString(Node tree) {
+        if (tree == null) {
             return null;
         }
 
@@ -109,19 +169,23 @@ public class Node {
     }
 
     @Override
-    public String toString() {
-        Node p = this;
-        StringBuilder sb = new StringBuilder();
-        sb.append('[');
-        while (p != null) {
-            sb.append(p.val).append(',');
-            p = p.next;
-            if (p == this) {
-                break;
-            }
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
         }
-        sb.deleteCharAt(sb.length() - 1);
-        sb.append(']');
-        return sb.toString();
+        if (!(obj instanceof Node)) {
+            return false;
+        }
+        return Objects.hash(this) == Objects.hash(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, next, random, left, right, neighbors);
+    }
+
+    @Override
+    public String toString() {
+        return listString();
     }
 }
