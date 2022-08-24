c40bfdeb71029891e973c7bc52a09641970c4c4b
weijiaduo <648620282@qq.com>
2022-08-25 00:57:18 +0800

    浏览器历史记录

---
+++ algorithm/src/test/java/com/wjd/practice/other/stack/BrowseHistoryTest.java
@@ -0,0 +1,69 @@
+package com.wjd.practice.other.stack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BrowseHistoryTest {
+
+    @Test
+    void current() {
+        BrowseHistory history = new BrowseHistory();
+        history.jump("baidu.com");
+        assertEquals("baidu.com", history.current());
+    }
+
+    @Test
+    void jump() {
+        BrowseHistory history = new BrowseHistory();
+        history.jump("baidu.com");
+        assertEquals("baidu.com", history.current());
+        history.jump("google.com");
+        assertEquals("google.com", history.current());
+    }
+
+    @Test
+    void forward() {
+        BrowseHistory history = new BrowseHistory();
+        history.jump("baidu.com");
+        assertEquals("baidu.com", history.current());
+        history.jump("google.com");
+        assertEquals("google.com", history.current());
+        assertEquals("baidu.com", history.backward());
+        assertEquals("baidu.com", history.current());
+        assertEquals("google.com", history.forward());
+        assertEquals("google.com", history.current());
+    }
+
+    @Test
+    void backward() {
+        BrowseHistory history = new BrowseHistory();
+        history.jump("baidu.com");
+        assertEquals("baidu.com", history.current());
+        history.jump("google.com");
+        assertEquals("google.com", history.current());
+        assertEquals("baidu.com", history.backward());
+        assertEquals("baidu.com", history.current());
+    }
+
+    @Test
+    void canForward() {
+        BrowseHistory history = new BrowseHistory();
+        history.jump("baidu.com");
+        assertEquals("baidu.com", history.current());
+        assertFalse(history.canForward());
+        history.jump("google.com");
+        history.backward();
+        assertTrue(history.canForward());
+    }
+
+    @Test
+    void canBackward() {
+        BrowseHistory history = new BrowseHistory();
+        history.jump("baidu.com");
+        assertEquals("baidu.com", history.current());
+        assertFalse(history.canBackward());
+        history.jump("google.com");
+        assertTrue(history.canBackward());
+    }
+}
