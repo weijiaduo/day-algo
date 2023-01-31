963e4b6565fd1d8dec3623260d8356ce7e80d85f
weijiaduo <648620282@qq.com>
2023-02-01 01:39:11 +0800

    红黑树：2-3树形式初版

---
+++ algorithm/src/main/java/com/wjd/structure/tree/redblack/RBTNode.java
@@ -0,0 +1,37 @@
+package com.wjd.structure.tree.redblack;
+
+/**
+ * 红黑树节点
+ *
+ * @author weijiaduo
+ * @since 2023/1/24
+ */
+public class RBTNode {
+
+    /**
+     * 左子节点
+     */
+    public RBTNode left;
+    /**
+     * 右子节点
+     */
+    public RBTNode right;
+    /**
+     * 节点值
+     */
+    public int val;
+    /**
+     * 节点颜色
+     */
+    boolean color = true;
+
+    public RBTNode(int val) {
+        this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        String type = color ? "r" : "b";
+        return type + val;
+    }
+}
