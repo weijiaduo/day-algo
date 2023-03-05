f251d4cfd3dafce16f621f4fcedeb4ece330a4b1
weijiaduo <648620282@qq.com>
2023-03-05 10:42:50 +0800

    无向图：基于文件构建图

---
+++ algorithm/src/main/java/com/wjd/algorithm/graph/undirected/build/GraphBuilder.java
@@ -0,0 +1,15 @@
+package com.wjd.algorithm.graph.undirected.build;
+
+import com.wjd.structure.graph.undirected.Graph;
+
+/**
+ * 无向图构建器
+ *
+ * @author weijiaduo
+ * @since 2023/3/5
+ */
+public interface GraphBuilder {
+
+    Graph build();
+
+}
