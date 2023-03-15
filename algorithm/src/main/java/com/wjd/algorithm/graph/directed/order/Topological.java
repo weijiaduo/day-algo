4b7030fc5bc7500fdc498ee9eb9def9e2faf0b86
weijiaduo <648620282@qq.com>
2023-03-16 00:41:36 +0800

    最短路径：无环加权有向图，基于拓扑排序的算法

--- algorithm/src/main/java/com/wjd/algorithm/graph/directed/order/Topological.java
+++ algorithm/src/main/java/com/wjd/algorithm/graph/directed/order/Topological.java
@@ -2,7 +2,9 @@ package com.wjd.algorithm.graph.directed.order;
 
 import com.wjd.algorithm.graph.Order;
 import com.wjd.algorithm.graph.directed.cycle.DirectedCycle;
+import com.wjd.algorithm.graph.directed.cycle.WeightedDirectedCycle;
 import com.wjd.structure.graph.directed.Digraph;
+import com.wjd.structure.graph.directed.WeightedDigraph;
 
 /**
  * 拓扑排序（基于深度优先的逆后序）
@@ -22,6 +24,13 @@ public class Topological implements Order {
             return;
         }
         orders = new ReversePostDFO(dg).order();
+    }
+
+    public Topological(WeightedDigraph wdg) {
+        if (new WeightedDirectedCycle(wdg).hasCycle()) {
+            return;
+        }
+        orders = new WeightedReversePostDFO(wdg).order();
     }
 
     @Override
