ed9f5278401d9fefe7ea902ae290e24321080c90
weijiaduo <648620282@qq.com>
2023-05-19 12:47:31 +0800

    41. 缺失的第一个整数

--- algorithm/src/main/java/com/wjd/practice/leetcode/array/statistics/FirstMissingPositive.java
+++ algorithm/src/main/java/com/wjd/practice/leetcode/array/statistics/FirstMissingPositive.java
@@ -1,6 +1,4 @@
-package com.wjd.practice.leetcode.array;
-
-import com.wjd.practice.Solution;
+package com.wjd.practice.leetcode.array.statistics;
 
 /**
  * 41. 缺失的第一个整数
@@ -12,66 +10,61 @@ import com.wjd.practice.Solution;
  * 输入：nums = [3,4,-1,1]
  * 输出：2
  * <p>
+ *
  * @since 2022/5/22
  */
-public class FirstMissingPositive implements Solution<Integer> {
-
-    @Override
-    public Integer solve(Object ...args) {
-        int[] nums = {1};
-        int result = firstMissingPositive(nums);
-        System.out.println(result);
-        return null;
-    }
+public class FirstMissingPositive {
 
     /**
-     * 索引排序法
+     * 思路：将数字放到它对应的索引位置上，如果索引和数字对不上，就说明丢失了
      * <p>
-     * 执行耗时:2 ms,击败了94.73% 的Java用户
-     * 内存消耗:96.9 MB,击败了70.02% 的Java用户
+     * 复杂度：时间 O(n) 空间 O(1)
+     * <p>
+     * 执行耗时:1 ms,击败了97.45% 的Java用户
+     * 内存消耗:57.9 MB,击败了10.69% 的Java用户
      */
     public int firstMissingPositive(int[] nums) {
-        int next = 0;
-        while (next < nums.length) {
-            int num = nums[next];
-            if (next == num - 1) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            if (i == num - 1) {
                 // 数字在正确的位置
-                next++;
                 continue;
             }
 
-            if (0 < num && num <= nums.length) {
-                // 正常数字范围内
+            if (0 < num && num <= n) {
                 int temp = nums[num - 1];
                 if (temp != num) {
                     // 避免重复数字出现死循环
                     nums[num - 1] = num;
-                    nums[next] = temp;
-                    // 交换后，next 不用变
-                    continue;
+                    nums[i] = temp;
+                    // 交换后，i 不用变
+                    i--;
                 }
             }
-            next++;
         }
 
         // 顺序遍历数字，找到缺失的数字
-        for (next = 1; next <= nums.length; next++) {
-            if (nums[next - 1] != next) {
-                return next;
+        for (int i = 1; i <= n; i++) {
+            if (nums[i - 1] != i) {
+                return i;
             }
         }
-        return next;
+        return n + 1;
     }
 
     /**
      * 官方解法
      * <p>
      * 这个解法还挺骚气的~~~~
+     * <p>
+     * 思路：将 nums[i] 对应索引位置的值改成负数，最后如果 nums[i] 不是负数，表明该位置的数字丢失了
+     * <p>
+     * 复杂度：时间 O(n) 空间 O(1)
      */
-    public int firstMissingPositive2(int[] nums) {
-        int n = nums.length;
-
+    private int firstMissingPositive2(int[] nums) {
         // 非法数值都改成超出正常范围的值
+        int n = nums.length;
         for (int i = 0; i < n; ++i) {
             if (nums[i] <= 0) {
                 nums[i] = n + 1;
