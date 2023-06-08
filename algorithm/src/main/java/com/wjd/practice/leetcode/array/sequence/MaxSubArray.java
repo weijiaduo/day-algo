167c8e44b3fa270d6f81abdee482dfd06b580b1d
weijiaduo <648620282@qq.com>
2023-06-08 22:43:37 +0800

    53. 最大子数组和

--- algorithm/src/main/java/com/wjd/practice/leetcode/array/sequence/MaxSubArray.java
+++ algorithm/src/main/java/com/wjd/practice/leetcode/array/sequence/MaxSubArray.java
@@ -1,33 +1,58 @@
 package com.wjd.practice.leetcode.array.sequence;
 
 /**
- * 53. 最大子序和
+ * 53. 最大子数组和
  * <p>
- * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * <p>
+ * 子数组 是数组中的一个连续部分。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组[4,-1,2,1] 的和最大，为6 。
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1]
+ * 输出：1
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：nums = [5,4,-1,7,8]
+ * 输出：23
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 10⁵
+ * -10⁴ <= nums[i] <= 10⁴
  *
  * @since 2021-05-29
  */
 public class MaxSubArray {
 
     /**
-     * 最大子序和
-     *
-     * @param nums 数组
-     * @return 最大和
+     * 思路：使用累加和来判断子数组要不要连接起来
+     * <p>
+     * 累加和 sum[i - 1] > 0 时，sum[i] = sum[i-1] + nums[i]
+     * <p>
+     * 累加和 sum[i - 1] <= 0时，sum[i] = nums[i]
+     * <p>
+     * 由于只用到前一个累加和，所以可以只用一个变量保存上一个累加和即可
+     * <p>
+     * 复杂度：时间 O(n) 空间 O(1)
+     * <p>
+     * 执行耗时:1 ms,击败了100.00% 的Java用户
+     * 内存消耗:56.3 MB,击败了16.69% 的Java用户
      */
     public int maxSubArray(int[] nums) {
-        int maxSum = Integer.MIN_VALUE;
-        int[] sums = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && sums[i - 1] >= 0) {
-                sums[i] = sums[i - 1] + nums[i];
-            } else {
-                sums[i] = nums[i];
-            }
-            if (sums[i] > maxSum) {
-                maxSum = sums[i];
-            }
+        int maxSum = nums[0], pre = 0;
+        for (int num : nums) {
+            pre = Math.max(pre + num, num);
+            maxSum = Math.max(pre, maxSum);
         }
         return maxSum;
     }
+
 }
