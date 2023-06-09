286afea06d4a9aad18a76ff62551fa84d2b3e749
weijiaduo <648620282@qq.com>
2023-06-09 12:40:54 +0800

    73. 矩阵置零

--- algorithm/src/main/java/com/wjd/practice/leetcode/array/matrix/SetMatrixZeroes.java
+++ algorithm/src/main/java/com/wjd/practice/leetcode/array/matrix/SetMatrixZeroes.java
@@ -3,11 +3,12 @@ package com.wjd.practice.leetcode.array.matrix;
 /**
  * 73. 矩阵置零
  * <p>
- * 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+ * 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。
+ * <p>
+ * 请使用 原地 算法。
  * <p>
  * 输入：matrix = [[1,1,1],[1,0,1],[1,1,1]]
  * 输出：[[1,0,1],[0,0,0],[1,0,1]]
- * <p>
  *
  * @since 2022/6/4
  */
@@ -21,6 +22,8 @@ public class SetMatrixZeroes {
 
     /**
      * 思路：额外的二维空间保存 0 的值
+     * <p>
+     * 复杂度：时间 O(nm(m+n)) 空间 O(mn)
      */
     public void matrixSetZeroes(int[][] matrix) {
         if (matrix.length == 0 || matrix[0].length == 0) {
@@ -49,6 +52,8 @@ public class SetMatrixZeroes {
 
     /**
      * 思路：额外的2个一维数组记录 0 值
+     * <p>
+     * 复杂度：时间 O(mn) 空间 O(m + n)
      * <p>
      * 执行耗时:1 ms,击败了37.14% 的Java用户
      * 内存消耗:42.6 MB,击败了93.23% 的Java用户
@@ -85,6 +90,8 @@ public class SetMatrixZeroes {
 
     /**
      * 思路：把0值所在的行头和列头标记为0，表明这一行和一列都需要设置成0
+     * <p>
+     * 复杂度：时间 O(mn) 空间 O(1)
      * <p>
      * 执行耗时:0 ms,击败了100.00% 的Java用户
      * 内存消耗:42.8 MB,击败了69.13% 的Java用户
