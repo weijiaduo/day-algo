cf9f8f2e41c24104d412eef05ba6d3c4a07ca3c2
weijiaduo <648620282@qq.com>
2023-06-12 12:47:46 +0800

    98. 验证二叉搜索树

--- algorithm/src/main/java/com/wjd/practice/leetcode/tree/validate/IsValidBST.java
+++ algorithm/src/main/java/com/wjd/practice/leetcode/tree/validate/IsValidBST.java
@@ -15,14 +15,13 @@ import com.wjd.structure.tree.binary.TreeNode;
  * <p>
  * 输入：root = [2,1,3]
  * 输出：true
- * <p>
  *
  * @since 2022/6/11
  */
 public class IsValidBST {
 
     public boolean isValidBST(TreeNode root) {
-        return isValid(root, null, null);
+        return divide(root, null, null);
     }
 
     /**
@@ -32,18 +31,20 @@ public class IsValidBST {
      * <p>
      * 特别需要注意边界值的判断 Integer.MAX_VALUE 和 Integer.MIN_VALUE
      * <p>
+     * 复杂度：时间 O(n) 空间 O(logn)
+     * <p>
      * 执行耗时:0 ms,击败了100.00% 的Java用户
      * 内存消耗:41.3 MB,击败了6.16% 的Java用户
      */
-    private boolean isValid(TreeNode root, Integer min, Integer max) {
+    private boolean divide(TreeNode root, Integer min, Integer max) {
         if (root == null) {
             return true;
         }
         if (min != null && root.val <= min || max != null && root.val >= max) {
             return false;
         }
-        return isValid(root.left, min, root.val)
-                && isValid(root.right, root.val, max);
+        return divide(root.left, min, root.val)
+                && divide(root.right, root.val, max);
     }
 
     TreeNode pre = null;
@@ -51,7 +52,7 @@ public class IsValidBST {
     /**
      * 思路：使用中序遍历验证，中序遍历结果是从小打大，则表示是有效的二叉搜索树
      * <p>
-     * 复杂度：时间 O(logn) 空间 O(logn)
+     * 复杂度：时间 O(n) 空间 O(logn)
      * <p>
      * 执行用时：0 ms, 在所有 Java 提交中击败了 100.00% 的用户
      * 内存消耗：40.8 MB, 在所有 Java 提交中击败了 89.53% 的用户
@@ -59,12 +60,12 @@ public class IsValidBST {
      * @param root 根节点
      * @return 是否是二叉搜索树
      */
-    private boolean inorderValid(TreeNode root) {
+    private boolean inorder(TreeNode root) {
         if (root == null) {
             return true;
         }
 
-        if (!inorderValid(root.left)) {
+        if (!inorder(root.left)) {
             return false;
         }
 
@@ -73,11 +74,7 @@ public class IsValidBST {
         }
 
         pre = root;
-        if (!inorderValid(root.right)) {
-            return false;
-        }
-
-        return true;
+        return inorder(root.right);
     }
 
 }
