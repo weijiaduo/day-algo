a2523570f53bc3800857ee2729d1e6255b5c61d5
weijiaduo <648620282@qq.com>
2023-07-05 03:28:52 +0800

    139. 单词拆分

--- practice/src/main/java/com/wjd/practice/leetcode/string/combination/WordBreak.java
+++ practice/src/main/java/com/wjd/practice/leetcode/string/combination/WordBreak.java
@@ -1,9 +1,8 @@
 package com.wjd.practice.leetcode.string.combination;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import com.wjd.practice.leetcode.TestCase;
+
+import java.util.*;
 
 /**
  * 139. 单词拆分
@@ -14,27 +13,52 @@ import java.util.Set;
  * <p>
  * 注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
  * <p>
+ * 示例 1：
+ * <p>
  * 输入: s = "leetcode", wordDict = ["leet", "code"]
  * 输出: true
  * 解释: 返回 true 因为 "leetcode" 可以由 "leet" 和 "code" 拼接成。
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以由 "apple" "pen" "apple" 拼接成。
+ * 注意，你可以重复使用字典中的单词。
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 300
+ * 1 <= wordDict.length <= 1000
+ * 1 <= wordDict[i].length <= 20
+ * s 和 wordDict[i] 仅有小写英文字母组成
+ * wordDict 中的所有字符串 互不相同
  *
  * @author weijiaduo
  * @since 2022/6/26
  */
 public class WordBreak {
 
-    public boolean wordBreak(String s, List<String> wordDict) {
-        // return dfs(s, 0, wordDict, new HashMap<>());
-        return dynamic(s, wordDict);
-    }
-
     /**
      * 思路：递归+记忆，不断递归判断子串是否可由字典拼成
+     * <p>
+     * 复杂度：时间 O(n^2 * m)，空间 O(n^2)
      * <p>
      * 执行耗时:1 ms,击败了99.64% 的Java用户
      * 内存消耗:41.4 MB,击败了64.82% 的Java用户
      */
-    private boolean dfs(String s, int i, List<String> wordDict, Map<Integer, Boolean> cache) {
+    @TestCase(input = {"leetcode", "[\"leet\", \"code\"]", "applepenapple", "[\"apple\", \"pen\"]"},
+            output = {"true", "true"})
+    public boolean meno(String s, List<String> wordDict) {
+        return meno(s, 0, wordDict, new HashMap<>());
+    }
+
+    private boolean meno(String s, int i, List<String> wordDict, Map<Integer, Boolean> cache) {
         Boolean result = cache.get(i);
         if (result != null) {
             return result;
@@ -48,7 +72,7 @@ public class WordBreak {
                     || !word.equals(s.substring(i, i + word.length()))) {
                 continue;
             }
-            if (dfs(s, i + word.length(), wordDict, cache)) {
+            if (meno(s, i + word.length(), wordDict, cache)) {
                 cache.put(i, true);
                 return true;
             }
@@ -65,6 +89,8 @@ public class WordBreak {
      * 执行耗时:6 ms,击败了74.67% 的Java用户
      * 内存消耗:41.8 MB,击败了12.75% 的Java用户
      */
+    @TestCase(input = {"leetcode", "[\"leet\", \"code\"]", "applepenapple", "[\"apple\", \"pen\"]"},
+            output = {"true", "true"})
     private boolean dynamic(String s, List<String> wordDict) {
         // 单词转成Set可提高速度
         Set<String> words = new HashSet<>(wordDict);
@@ -97,6 +123,8 @@ public class WordBreak {
      * 执行耗时:1 ms,击败了99.64% 的Java用户
      * 内存消耗:39.8 MB,击败了91.17% 的Java用户
      */
+    @TestCase(input = {"leetcode", "[\"leet\", \"code\"]", "applepenapple", "[\"apple\", \"pen\"]"},
+            output = {"true", "true"})
     private boolean dynamic2(String s, List<String> wordDict) {
         // 单词转成Set可提高速度
         Set<String> words = new HashSet<>(wordDict.size());
