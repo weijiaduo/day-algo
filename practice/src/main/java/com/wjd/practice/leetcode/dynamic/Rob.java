be7dda84f8bc6527b00fde0d74a870e0543d7e4d
weijiaduo <648620282@qq.com>
2023-07-04 12:32:51 +0800

    198. 打家劫舍

--- practice/src/main/java/com/wjd/practice/leetcode/dynamic/Rob.java
+++ practice/src/main/java/com/wjd/practice/leetcode/dynamic/Rob.java
@@ -1,5 +1,7 @@
 package com.wjd.practice.leetcode.dynamic;
 
+import com.wjd.practice.leetcode.TestCase;
+
 /**
  * 198. 打家劫舍
  * <p>
@@ -9,48 +11,66 @@ package com.wjd.practice.leetcode.dynamic;
  * <p>
  * 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
  * <p>
- * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+ * 给定一个代表每个房屋存放金额的非负整数数组，
+ * <p>
+ * 计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+ * <p>
+ * 示例 1：
  * <p>
  * 输入：[1,2,3,1]
  * 输出：4
  * 解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
  * 偷窃到的最高金额 = 1 + 3 = 4 。
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：[2,7,9,3,1]
+ * 输出：12
+ * 解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+ * 偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 400
  *
  * @author weijiaduo
  * @since 2022/7/9
  */
 public class Rob {
 
-    public int rob(int[] nums) {
-        // return dynamic(nums);
-        return dynamic2(nums);
-    }
-
     /**
-     * 思路：动态规划，当前位置，可以取，也可以不取，动态记录这2种状态，直到最后算出最大值
+     * 思路：动态规划，二维动态数组
+     * <p>
+     * 每个房子，可以分为2种情况：
+     * <p>
+     * 1、取当前房子
+     * 2、不取当前房子
+     * <p>
+     * 记录该房子这两种状态下的最大值，然后得得转移公式
      * <p>
      * 复杂度：时间 O(n) 空间 O(n)
      * <p>
      * 执行耗时:0 ms,击败了100.00% 的Java用户
      * 内存消耗:39 MB,击败了28.92% 的Java用户
-     *
-     * @param nums 数组
-     * @return 结果
      */
-    private int dynamic(int[] nums) {
+    @TestCase(input = {"[1,2,3,1]", "[2,7,9,3,1]"},
+            output = {"4", "12"})
+    private int dynamic2(int[] nums) {
         int n = nums.length;
-        if (n <= 0) {
+        if (n == 0) {
             return 0;
         }
 
-        // 初始化状态
+        // 创建动态数组
+        // dp[i][0] 表示不取，dp[i][1] 表示取
         int[][] dp = new int[n][2];
-        // 不取当前位置
+
+        // 初始化状态
         dp[0][0] = 0;
-        // 取当前位置
         dp[0][1] = nums[0];
 
-        // 动态计算
+        // 状态转移
         for (int i = 1; i < n; i++) {
             // 不取当前位置，上一次可以取也可以不取
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
@@ -62,19 +82,63 @@ public class Rob {
     }
 
     /**
-     * 思路：压缩动态规划，由于动态规划只用到了上一个值，所以无需用数组保存，用2个变量即可
+     * 思路：动态规划，一维动态数组
+     * <p>
+     * dp[i] 表示前 i 间房子得到的最大值
+     * <p>
+     * 那么由于相邻房子不能一起偷，所以可分为2种情况：
+     * <p>
+     * 1、取当前房子
+     * 2、不取当前房子
+     * <p>
+     * 由此可得到转移公式：
+     * <p>
+     * dp[i] = max(dp[i - 1], dp[i -2] + nums[i])
+     * <p>
+     * 复杂度：时间 O(n) 空间 O(n)
+     * <p>
+     * 执行耗时:0 ms,击败了100.00% 的Java用户
+     * 内存消耗:38.9 MB,击败了51.91% 的Java用户
+     */
+    @TestCase(input = {"[1,2,3,1]", "[2,7,9,3,1]"},
+            output = {"4", "12"})
+    private int dynamic1(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return nums[0];
+        }
+
+        // 创建动态数组
+        int[] dp = new int[n];
+        // 初始化状态
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        // 状态转移
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        return dp[n - 1];
+    }
+
+    /**
+     * 思路：动态规划，滚动数组
+     * <p>
+     * 由于动态规划只用到了上一个值，所以无需用数组保存，用2个变量即可
      * <p>
      * 复杂度：时间 O(n) 空间 O(1)
      * <p>
      * 执行耗时:0 ms,击败了100.00% 的Java用户
      * 内存消耗:38.7 MB,击败了65.65% 的Java用户
-     *
-     * @param nums 数组
-     * @return 结果
      */
-    private int dynamic2(int[] nums) {
+    @TestCase(input = {"[1,2,3,1]", "[2,7,9,3,1]"},
+            output = {"4", "12"})
+    private int dynamic0(int[] nums) {
         int n = nums.length;
-        if (n <= 0) {
+        if (n == 0) {
             return 0;
         }
 
