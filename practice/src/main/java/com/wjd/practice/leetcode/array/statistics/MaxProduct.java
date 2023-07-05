3fc0ff120a6969f0bcc61d0afdab7951e35c30ea
weijiaduo <648620282@qq.com>
2023-07-05 15:24:03 +0800

    152. 乘积最大子数组

--- practice/src/main/java/com/wjd/practice/leetcode/array/statistics/MaxProduct.java
+++ practice/src/main/java/com/wjd/practice/leetcode/array/statistics/MaxProduct.java
@@ -1,5 +1,7 @@
 package com.wjd.practice.leetcode.array.statistics;
 
+import com.wjd.practice.leetcode.TestCase;
+
 /**
  * 152. 乘积最大子数组
  * <p>
@@ -9,23 +11,28 @@ package com.wjd.practice.leetcode.array.statistics;
  * <p>
  * 子数组 是数组的连续子序列。
  * <p>
+ * 示例 1:
+ * <p>
  * 输入: nums = [2,3,-2,4]
  * 输出: 6
  * 解释:子数组 [2,3] 有最大乘积 6。
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: nums = [-2,0,-1]
+ * 输出: 0
+ * 解释:结果不能为 2, 因为 [-2,-1] 不是子数组。
+ * <p>
+ * 提示:
+ * <p>
+ * 1 <= nums.length <= 2 * 10⁴
+ * -10 <= nums[i] <= 10
+ * nums 的任何前缀或后缀的乘积都 保证 是一个 32-位 整数
  *
  * @author weijiaduo
  * @since 2022/6/30
  */
 public class MaxProduct {
-
-    public int maxProduct(int[] nums) {
-        // if (nums.length == 0) {
-        //     return 0;
-        // }
-        // int[] result = dfs(nums, 0);
-        // return result[2];
-        return dynamic(nums);
-    }
 
     /**
      * 思路：递归，分为2种情况：1、仅当前元素 2、当前元素*连续子数组
@@ -35,6 +42,16 @@ public class MaxProduct {
      * 执行耗时:1 ms,击败了95.21% 的Java用户
      * 内存消耗:42.5 MB,击败了5.02% 的Java用户
      */
+    @TestCase(input = {"[2,3,-2,4]", "[-2,0,-1]"},
+            output = {"6", "0"})
+    private int dfs(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] result = dfs(nums, 0);
+        return result[2];
+    }
+
     private int[] dfs(int[] nums, int i) {
         if (i == nums.length) {
             return new int[]{1, 1, Integer.MIN_VALUE};
@@ -66,7 +83,9 @@ public class MaxProduct {
      * 执行耗时:2 ms,击败了66.03% 的Java用户
      * 内存消耗:41.7 MB,击败了30.69% 的Java用户
      */
-    private int dynamic(int[] nums) {
+    @TestCase(input = {"[2,3,-2,4]", "[-2,0,-1]"},
+            output = {"6", "0"})
+    private int dynamic0(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
