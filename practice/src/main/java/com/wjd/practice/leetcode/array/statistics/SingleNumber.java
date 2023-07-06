0be7bce497e4a23d94d41fe2a144030a0905e541
weijiaduo <648620282@qq.com>
2023-07-07 02:58:08 +0800

    136. 只出现一次的数字

--- practice/src/main/java/com/wjd/practice/leetcode/array/statistics/SingleNumber.java
+++ practice/src/main/java/com/wjd/practice/leetcode/array/statistics/SingleNumber.java
@@ -1,31 +1,57 @@
 package com.wjd.practice.leetcode.array.statistics;
 
+import com.wjd.practice.leetcode.TestCase;
+
 /**
  * 136. 只出现一次的数字
  * <p>
- * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。
+ * 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。
  * <p>
  * 找出那个只出现了一次的元素。
  * <p>
- * 说明：
+ * 你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
  * <p>
- * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ * 示例 1 ：
+ * <p>
+ * 输入：nums = [2,2,1]
+ * 输出：1
+ * <p>
+ * 示例 2 ：
+ * <p>
+ * 输入：nums = [4,1,2,1,2]
+ * 输出：4
+ * <p>
+ * 示例 3 ：
+ * <p>
+ * 输入：nums = [1]
+ * 输出：1
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 3 * 10⁴
+ * -3 * 10⁴ <= nums[i] <= 3 * 10⁴
+ * 除了某个元素只出现一次以外，其余每个元素均出现两次。
  *
  * @since 2021-06-28
  */
 public class SingleNumber {
 
     /**
-     * 只出现一次的数字
-     *
-     * @param nums 数组
-     * @return 数字
+     * 思路：异或，将所有数字异或起来，就会只剩下只出现一次的数字
+     * <p>
+     * 复杂度：时间 O(n) 空间 O(1)
+     * <p>
+     * 执行耗时:0 ms,击败了100.00% 的Java用户
+     * 内存消耗:43.7 MB,击败了5.01% 的Java用户
      */
-    public int findSingleNumber(int[] nums) {
-        int result = 0;
+    @TestCase(input = {"[2,2,1]", "[4,1,2,1,2]", "[1]"},
+            output = {"1", "4", "1"})
+    public int xor(int[] nums) {
+        int ans = 0;
         for (int num : nums) {
-            result ^= num;
+            ans ^= num;
         }
-        return result;
+        return ans;
     }
+
 }
