df313eb9c27b4897e690881b3b8cb5ed744a6447
weijiaduo <648620282@qq.com>
2023-07-15 09:21:52 +0800

    18. 四数之和

--- practice/src/main/java/com/wjd/practice/leetcode/array/combination/FourSum.java
+++ practice/src/main/java/com/wjd/practice/leetcode/array/combination/FourSum.java
@@ -1,5 +1,7 @@
 package com.wjd.practice.leetcode.array.combination;
 
+import com.wjd.practice.leetcode.TestCase;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,95 +9,124 @@ import java.util.List;
 /**
  * 18. 四数之和
  * <p>
- * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，
+ * 给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。
  * <p>
- * 判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？
+ * 请你找出并返回满足下述全部条件且不重复的四元组
  * <p>
- * 找出所有满足条件且不重复的四元组。
+ * [nums[a], nums[b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
  * <p>
- * 注意：答案中不可以包含重复的四元组。
+ * 0 <= a, b, c, d < n
+ * a、b、c 和 d 互不相同
+ * nums[a] + nums[b] + nums[c] + nums[d] == target
  * <p>
- * 0 <= nums.length <= 200
- * -109 <= nums[i] <= 109
- * -109 <= target <= 109
+ * 你可以按 任意顺序 返回答案 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,0,-1,0,-2,2], target = 0
+ * 输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：nums = [2,2,2,2,2], target = 8
+ * 输出：[[2,2,2,2]]
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 200
+ * -10⁹ <= nums[i] <= 10⁹
+ * -10⁹ <= target <= 10⁹
  *
+ * @author weijiaduo
  * @since 2021-07-01
  */
 public class FourSum {
 
     /**
-     * 四数之和
-     *
-     * @param nums   数组
-     * @param target 目标值
-     * @return 四元组列表
+     * 思路：排序 + 回溯 + 双指针
+     * <p>
+     * 排序后，外面 2 层循环，里面 1 层双指针遍历
+     * <p>
+     * 复杂度：时间 O(n^3) 空间 O(logn)
+     * <p>
+     * 执行耗时:15 ms,击败了61.48% 的Java用户
+     * 内存消耗:43.2 MB,击败了9.69% 的Java用户
      */
-    private List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
+    @TestCase(input = {"[1,0,-1,0,-2,2]", "0", "[2,2,2,2,2]", "8", "[1000000000,1000000000,1000000000,1000000000]", "-294967296", "[1,-2,-5,-4,-3,3,3,5]", "-11"},
+            output = {"[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]", "[[2,2,2,2]]", "[]", "[[-5,-4,-3,1]]"})
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
         if (nums.length < 4) {
-            return result;
+            return ans;
         }
         Arrays.sort(nums);
         List<Integer> values = new ArrayList<>(4);
         for (int i = 0; i < nums.length - 3; i++) {
+            // 升序剪枝
+            if (nums[i] >= 0 && nums[i] > target) {
+                break;
+            }
             // 去重
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             values.add(nums[i]);
-            List<List<Integer>> sumList = threeSum(nums, target - nums[i], i + 1, nums.length - 1, values);
-            if (sumList.size() > 0) {
-                result.addAll(sumList);
-            }
+            ans.addAll(threeSum(nums, (long) target - nums[i], i + 1, nums.length - 1, values));
             values.remove(values.size() - 1);
         }
-        return result;
+        return ans;
     }
 
     /**
-     * 三数之和
+     * 回溯，3 数之和
      *
-     * @param nums 数组
-     * @return 所有三元组
+     * @param nums   升序数组
+     * @param target 三数和
+     * @param start  [start, end]
+     * @param end    [start, end]
+     * @param values 值集合
      */
-    private List<List<Integer>> threeSum(int[] nums, int target, int start, int end, List<Integer> values) {
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i = start; i < nums.length - 2; i++) {
+    private List<List<Integer>> threeSum(int[] nums, long target, int start, int end, List<Integer> values) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = start; i < end - 1; i++) {
+            // 升序剪枝
+            if (nums[i] >= 0 && nums[i] > target) {
+                break;
+            }
             // 去重
             if (i > start && nums[i] == nums[i - 1]) {
                 continue;
             }
             values.add(nums[i]);
-            List<List<Integer>> sumList = twoSum(nums, target - nums[i], i + 1, nums.length - 1, values);
-            if (sumList.size() > 0) {
-                result.addAll(sumList);
-            }
+            ans.addAll(twoSum(nums, target - nums[i], i + 1, end, values));
             values.remove(values.size() - 1);
         }
-        return result;
+        return ans;
     }
 
     /**
-     * 求2数之和
+     * 双指针，2 数之和
      *
      * @param nums   升序数组
      * @param target 两数和
-     * @param start  起始地址
-     * @param end    结束地址
+     * @param start  [start, end]
+     * @param end    [start, end]
+     * @param values 值集合
      * @return 两数，或null
      */
-    private List<List<Integer>> twoSum(int[] nums, int target, int start, int end, List<Integer> values) {
-        List<List<Integer>> result = new ArrayList<>();
+    private List<List<Integer>> twoSum(int[] nums, long target, int start, int end, List<Integer> values) {
+        List<List<Integer>> ans = new ArrayList<>();
         int lp = start, rp = end;
         int size = values.size() + 2;
         while (lp < rp) {
-            int temp = nums[lp] + nums[rp];
+            long temp = (long) nums[lp] + nums[rp];
             if (temp == target) {
-                List<Integer> sumList = new ArrayList<>(size);
-                sumList.addAll(values);
-                sumList.add(nums[lp]);
-                sumList.add(nums[rp]);
-                result.add(sumList);
+                List<Integer> list = new ArrayList<>(size);
+                list.addAll(values);
+                list.add(nums[lp]);
+                list.add(nums[rp]);
+                ans.add(list);
+
                 // 去重
                 while (lp < rp && nums[lp] == nums[++lp]) ;
                 while (lp < rp && nums[rp] == nums[--rp]) ;
@@ -105,6 +136,7 @@ public class FourSum {
                 rp--;
             }
         }
-        return result;
+        return ans;
     }
+
 }
