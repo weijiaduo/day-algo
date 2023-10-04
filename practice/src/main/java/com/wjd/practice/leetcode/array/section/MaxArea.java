da292a8a2c46e26dfa930444429eddbd956882d6
weijiaduo <648620282@qq.com>
2023-10-04 12:14:30 +0800

    11. 盛最多水的容器

--- practice/src/main/java/com/wjd/practice/leetcode/array/section/MaxArea.java
+++ practice/src/main/java/com/wjd/practice/leetcode/array/section/MaxArea.java
@@ -1,15 +1,38 @@
 package com.wjd.practice.leetcode.array.section;
 
+import com.wjd.practice.leetcode.TestCase;
+
 /**
  * 11. 盛最多水的容器
  * <p>
- * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点(i,ai) 。
+ * 给定一个长度为 n 的整数数组 height 。
  * <p>
- * 在坐标内画 n 条垂直线，垂直线 i的两个端点分别为(i,ai) 和 (i, 0) 。
+ * 有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
  * <p>
- * 找出其中的两条线，使得它们与x轴共同构成的容器可以容纳最多的水。
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * <p>
+ * 返回容器可以储存的最大水量。
  * <p>
  * 说明：你不能倾斜容器。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。
+ * <p>
+ * 在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为49。
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：height = [1,1]
+ * 输出：1
+ * <p>
+ * 提示：
+ * <p>
+ * n == height.length
+ * 2 <= n <= 10⁵
+ * 0 <= height[i] <= 10⁴
  *
  * @since 2021-05-29
  */
@@ -24,7 +47,7 @@ public class MaxArea {
      * <p>
      * 1、若移动短板，容器的高度 Min(h[l], h[r]) 可能会变大或变小，最终容量可能增大或减少
      * <p>
-     * 2、若移动长版，容器的高度 Min(h[l], h[r]) 会保持不变或变小，最终容量可能不变或减少
+     * 2、若移动长板，容器的高度 Min(h[l], h[r]) 会保持不变或变小，最终容量可能不变或减少
      * <p>
      * 所以，移动长板容量肯定是不会增大的，但是移动短板就有可能增大，因此每次移动都应该移动短板
      * <p>
@@ -36,6 +59,8 @@ public class MaxArea {
      * @param height 数组
      * @return 最大值
      */
+    @TestCase(input = {"[1,8,6,2,5,4,8,3,7]", "[1,1]"},
+            output = {"49", "1"})
     public int doublePoint(int[] height) {
         int lp = 0, rp = height.length - 1;
         int maxArea = 0;
@@ -54,12 +79,18 @@ public class MaxArea {
     }
 
     /**
-     * 动态规划（超时）
+     * 思路：动态规划
+     * <p>
+     * 复杂度：时间 O(n^2) 空间 O(n)
+     * <p>
+     * 超出时间限制
      *
      * @param height 数组
      * @return 最大值
      */
-    private int dynamic(int[] height) {
+    @TestCase(input = {"[1,8,6,2,5,4,8,3,7]", "[1,1]"},
+            output = {"49", "1"})
+    public int dynamic(int[] height) {
         int[] areas = new int[height.length];
         for (int i = 1; i < height.length; i++) {
             // 最大值不包括i在内
