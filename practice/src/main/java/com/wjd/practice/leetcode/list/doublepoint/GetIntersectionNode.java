57ef67617f3daae6982212663ecdf38a9a302ac6
weijiaduo <648620282@qq.com>
2023-10-04 16:59:17 +0800

    160. 相交链表

--- practice/src/main/java/com/wjd/practice/leetcode/list/doublepoint/GetIntersectionNode.java
+++ practice/src/main/java/com/wjd/practice/leetcode/list/doublepoint/GetIntersectionNode.java
@@ -1,5 +1,6 @@
 package com.wjd.practice.leetcode.list.doublepoint;
 
+import com.wjd.practice.leetcode.TestCase;
 import com.wjd.practice.leetcode.structure.ListNode;
 
 /**
@@ -9,9 +10,63 @@ import com.wjd.practice.leetcode.structure.ListNode;
  * <p>
  * 如果两个链表不存在相交节点，返回 null 。
  * <p>
+ * 图示两个链表在节点 c1 开始相交：
+ * <p>
  * 题目数据 保证 整个链式结构中不存在环。
  * <p>
  * 注意，函数返回结果后，链表必须 保持其原始结构 。
+ * <p>
+ * 自定义评测：
+ * <p>
+ * 评测系统 的输入如下（你设计的程序 不适用 此输入）：
+ * <p>
+ * intersectVal - 相交的起始节点的值。如果不存在相交节点，这一值为 0
+ * listA - 第一个链表
+ * listB - 第二个链表
+ * skipA - 在 listA 中（从头节点开始）跳到交叉节点的节点数
+ * skipB - 在 listB 中（从头节点开始）跳到交叉节点的节点数
+ * <p>
+ * 评测系统将根据这些输入创建链式数据结构，并将两个头节点 headA 和 headB 传递给你的程序。
+ * <p>
+ * 如果程序能够正确返回相交节点，那么你的解决方案将被 视作正确答案 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+ * 输出：Intersected at '8'
+ * 解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
+ * 从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,6,1,8,4,5]。
+ * 在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ * — 请注意相交节点的值不为 1，因为在链表 A 和链表 B 之中值为 1 的节点 (A 中第二个节点和 B 中第三个节点) 是不同的节点。
+ * <p>
+ * 换句话说，它们在内存中指向两个不同的位置，而链表 A 和链表 B 中值为 8 的节点 (A 中第三个节点，B 中第四个节点) 在内存中指向相同的位置。
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：intersectVal= 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+ * 输出：Intersected at '2'
+ * 解释：相交节点的值为 2 （注意，如果两个链表相交则不能为 0）。
+ * 从各自的表头开始算起，链表 A 为 [1,9,1,2,4]，链表 B 为 [3,2,4]。
+ * 在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+ * 输出：null
+ * 解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。
+ * 由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+ * 这两个链表不相交，因此返回 null 。
+ * <p>
+ * 提示：
+ * <p>
+ * listA 中节点数目为 m
+ * listB 中节点数目为 n
+ * 1 <= m, n <= 3 * 10⁴
+ * 1 <= Node.val <= 10⁵
+ * 0 <= skipA <= m
+ * 0 <= skipB <= n
+ * 如果 listA 和 listB 没有交点，intersectVal 为 0
+ * 如果 listA 和 listB 有交点，intersectVal == listA[skipA] == listB[skipB]
  *
  * @author weijiaduo
  * @since 2022/7/2
@@ -26,6 +81,8 @@ public class GetIntersectionNode {
      * 执行耗时:1 ms,击败了97.85% 的Java用户
      * 内存消耗:44.2 MB,击败了56.31% 的Java用户
      */
+    @TestCase(input = {"[4,1,8,4,5]", "[5,6,1,8,4,5]", "[1,9,1,2,4]", "[3,2,4]", "[2,6,4]", "[1,5]"},
+            output = {"[8,4,5]", "[2,4]", ""})
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         // 分别计算出长短链表的长度
         int m = 0, n = 0;
@@ -68,10 +125,16 @@ public class GetIntersectionNode {
     /**
      * 官解：这么简洁的吗
      * <p>
+     * 思路：假装将两条链表拼接起来，一条是 A+B，一条是 B+A
+     * <p>
+     * 这样遍历两条链表，最终两指针相等的地方就是链表的交接处
+     * <p>
      * 复杂度：时间O(m + n) 空间 O(1)
      * <p>
      * 执行耗时:1 ms,击败了97.85% 的Java用户 内存消耗:44.3 MB,击败了43.16% 的Java用户
      */
+    @TestCase(input = {"[4,1,8,4,5]", "[5,6,1,8,4,5]", "[1,9,1,2,4]", "[3,2,4]", "[2,6,4]", "[1,5]"},
+            output = {"[8,4,5]", "[2,4]", ""})
     private ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) {
             return null;
