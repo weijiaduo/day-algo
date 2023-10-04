a9fc5bf4ebab1c0982e4fdaf50e2d80b15d41e5d
weijiaduo <648620282@qq.com>
2023-10-05 00:00:01 +0800

    22. 括号生成

--- practice/src/main/java/com/wjd/practice/leetcode/backtrack/GenerateParenthesis.java
+++ practice/src/main/java/com/wjd/practice/leetcode/backtrack/GenerateParenthesis.java
@@ -1,5 +1,7 @@
 package com.wjd.practice.leetcode.backtrack;
 
+import com.wjd.practice.leetcode.TestCase;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,15 +28,6 @@ import java.util.List;
  */
 public class GenerateParenthesis {
 
-    public List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList<>();
-        // StringBuilder str = new StringBuilder(n * 2);
-        // backtrack(n, n, str, ans);
-        char[] chars = new char[2 * n];
-        backtrack(n, n, chars, 0, ans);
-        return ans;
-    }
-
     /**
      * 思路：回溯法，枚举 2n 个位置，每个位置的可选值有：左括号/右括号
      * <p>
@@ -46,13 +39,25 @@ public class GenerateParenthesis {
      * <p>
      * 执行耗时:0 ms,击败了100.00% 的Java用户
      * 内存消耗:41.1 MB,击败了82.07% 的Java用户
+     */
+    @TestCase(input = {"3", "1"},
+            output = {"[\"((()))\",\"(()())\",\"(())()\",\"()(())\",\"()()()\"]", "[\"()\"]"})
+    public List<String> backtrack1(int n) {
+        List<String> ans = new ArrayList<>();
+        StringBuilder str = new StringBuilder(n * 2);
+        backtrack1(n, n, str, ans);
+        return ans;
+    }
+
+    /**
+     * 回溯法，枚举 2n 个位置，每个位置的可选值有：左括号/右括号
      *
      * @param left  剩余没用的左括号
      * @param right 剩余没用的右括号
      * @param str   当前括号序列
      * @param ans   不同排列结果
      */
-    private void backtrack(int left, int right, StringBuilder str, List<String> ans) {
+    private void backtrack1(int left, int right, StringBuilder str, List<String> ans) {
         if (left == 0 && right == 0) {
             ans.add(str.toString());
             return;
@@ -64,13 +69,13 @@ public class GenerateParenthesis {
         // 左括号还有剩余
         if (left > 0) {
             str.append('(');
-            backtrack(left - 1, right, str, ans);
+            backtrack1(left - 1, right, str, ans);
             str.deleteCharAt(str.length() - 1);
         }
         // 右括号还有剩余
         if (right > 0) {
             str.append(')');
-            backtrack(left, right - 1, str, ans);
+            backtrack1(left, right - 1, str, ans);
             str.deleteCharAt(str.length() - 1);
         }
     }
@@ -86,6 +91,18 @@ public class GenerateParenthesis {
      * <p>
      * 执行耗时:0 ms,击败了100.00% 的Java用户
      * 内存消耗:40.7 MB,击败了94.67% 的Java用户
+     */
+    @TestCase(input = {"3", "1"},
+            output = {"[\"((()))\",\"(()())\",\"(())()\",\"()(())\",\"()()()\"]", "[\"()\"]"})
+    public List<String> backtrack2(int n) {
+        List<String> ans = new ArrayList<>();
+        char[] chars = new char[2 * n];
+        backtrack2(n, n, chars, 0, ans);
+        return ans;
+    }
+
+    /**
+     * 回溯法，枚举 2n 个位置，每个位置的可选值有：左括号/右括号
      *
      * @param left  剩余没用的左括号
      * @param right 剩余没用的右括号
@@ -93,7 +110,7 @@ public class GenerateParenthesis {
      * @param index 当前索引
      * @param ans   不同排列结果
      */
-    private void backtrack(int left, int right, char[] chars, int index, List<String> ans) {
+    private void backtrack2(int left, int right, char[] chars, int index, List<String> ans) {
         if (left == 0 && right == 0) {
             ans.add(new String(chars));
             return;
@@ -105,12 +122,12 @@ public class GenerateParenthesis {
         // 左括号还有剩余
         if (left > 0) {
             chars[index] = '(';
-            backtrack(left - 1, right, chars, index + 1, ans);
+            backtrack2(left - 1, right, chars, index + 1, ans);
         }
         // 右括号还有剩余
         if (right > 0) {
             chars[index] = ')';
-            backtrack(left, right - 1, chars, index + 1, ans);
+            backtrack2(left, right - 1, chars, index + 1, ans);
         }
     }
 
