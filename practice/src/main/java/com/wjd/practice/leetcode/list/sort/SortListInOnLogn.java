36bdc0620e24319e665084a321726928e50c1848
weijiaduo <648620282@qq.com>
2023-10-04 18:02:48 +0800

    148. 排序链表

--- practice/src/main/java/com/wjd/practice/leetcode/list/sort/SortListInOnLogn.java
+++ practice/src/main/java/com/wjd/practice/leetcode/list/sort/SortListInOnLogn.java
@@ -1,5 +1,6 @@
 package com.wjd.practice.leetcode.list.sort;
 
+import com.wjd.practice.leetcode.TestCase;
 import com.wjd.practice.leetcode.structure.ListNode;
 
 /**
@@ -7,19 +8,30 @@ import com.wjd.practice.leetcode.structure.ListNode;
  * <p>
  * 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
  * <p>
- * 进阶：你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+ * 示例 1：
+ * <p>
+ * 输入：head = [4,2,1,3]
+ * 输出：[1,2,3,4]
+ * <p>
+ * 示例 2：
  * <p>
  * 输入：head = [-1,5,3,4,0]
  * 输出：[-1,0,3,4,5]
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：head = []
+ * 输出：[]
+ * <p>
+ * 提示：
+ * <p>
+ * 链表中节点的数目在范围 [0, 5 * 10⁴] 内
+ * -10⁵ <= Node.val <= 10⁵
  *
  * @author weijiaduo
  * @since 2022-06-27
  */
 public class SortListInOnLogn {
-
-    public ListNode sortList(ListNode head) {
-        return mergeSort(head);
-    }
 
     /**
      * 思路：归并，分治处理子链表，再将它们合并起来
@@ -29,7 +41,9 @@ public class SortListInOnLogn {
      * 执行耗时:10 ms,击败了91.68% 的Java用户
      * 内存消耗:54.8 MB,击败了20.31% 的Java用户
      */
-    private ListNode mergeSort(ListNode head) {
+    @TestCase(input = {"[4,2,1,3]", "[-1,5,3,4,0]", "[]"},
+            output = {"[1,2,3,4]", "[-1,0,3,4,5]", "[]"})
+    public ListNode mergeSort(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
@@ -42,9 +56,9 @@ public class SortListInOnLogn {
         }
 
         // 分别对2条链表进行排序
-        ListNode right = sortList(slow.next);
+        ListNode right = mergeSort(slow.next);
         slow.next = null;
-        ListNode left = sortList(head);
+        ListNode left = mergeSort(head);
 
         // 合并排序好的2条链表
         return merge(left, right);
