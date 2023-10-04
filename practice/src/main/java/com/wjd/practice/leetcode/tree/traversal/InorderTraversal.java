cb4a83d763af09251c15da52c2062c189c4d3317
weijiaduo <648620282@qq.com>
2023-10-04 20:40:48 +0800

    94. 二叉树的中序遍历

--- practice/src/main/java/com/wjd/practice/leetcode/tree/traversal/InorderTraversal.java
+++ practice/src/main/java/com/wjd/practice/leetcode/tree/traversal/InorderTraversal.java
@@ -1,5 +1,6 @@
 package com.wjd.practice.leetcode.tree.traversal;
 
+import com.wjd.practice.leetcode.TestCase;
 import com.wjd.structure.tree.binary.TreeNode;
 
 import java.util.ArrayList;
@@ -12,27 +13,46 @@ import java.util.List;
  * <p>
  * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
  * <p>
+ * 示例 1：
+ * <p>
  * 输入：root = [1,null,2,3]
  * 输出：[1,3,2]
  * <p>
+ * 示例 2：
+ * <p>
+ * 输入：root = []
+ * 输出：[]
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：root = [1]
+ * 输出：[1]
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
  *
  * @since 2022/6/11
  */
 public class InorderTraversal {
 
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        // dfs(root, ans);
-        ans = iterator(root);
-        return ans;
-    }
-
     /**
-     * 递归法
+     * 思路：深度优先遍历，先左再中后右
+     * <p>
+     * 复杂度：时间 O(n) 空间 O(logn)
      * <p>
      * 执行耗时:0 ms,击败了100.00% 的Java用户
      * 内存消耗:39.8 MB,击败了10.36% 的Java用户
      */
+    @TestCase(input = {"[1,null,2,3]", "[]", "[1]"},
+            output = {"[1,3,2]", "[]", "[1]"})
+    public List<Integer> dfs(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        dfs(root, ans);
+        return ans;
+    }
+
     private void dfs(TreeNode node, List<Integer> ans) {
         if (node == null) {
             return;
@@ -43,14 +63,16 @@ public class InorderTraversal {
     }
 
     /**
-     * 迭代法
+     * 思路：迭代法，使用栈保存根节点，优先遍历左子树
      * <p>
-     * 思路：使用栈保存根节点，优先遍历左子树
+     * 复杂度：时间 O(n) 空间 O(n)
      * <p>
      * 执行耗时:0 ms,击败了100.00% 的Java用户
      * 内存消耗:40.1 MB,击败了5.13% 的Java用户
      */
-    private List<Integer> iterator(TreeNode root) {
+    @TestCase(input = {"[1,null,2,3]", "[]", "[1]"},
+            output = {"[1,3,2]", "[]", "[1]"})
+    public List<Integer> iterator(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         Deque<TreeNode> stack = new LinkedList<>();
         TreeNode node = root;
