c5eba03ffd5d699782bbf131427fca4c426cd3b6
weijiaduo <648620282@qq.com>
2023-10-04 17:50:26 +0800

    24. 两两交换链表中的节点

--- practice/src/main/java/com/wjd/practice/leetcode/list/transform/SwapPairs.java
+++ practice/src/main/java/com/wjd/practice/leetcode/list/transform/SwapPairs.java
@@ -1,11 +1,14 @@
 package com.wjd.practice.leetcode.list.transform;
 
+import com.wjd.practice.leetcode.TestCase;
 import com.wjd.practice.leetcode.structure.ListNode;
 
 /**
  * 24. 两两交换链表中的节点
  * <p>
- * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+ * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。
+ * <p>
+ * 你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
  * <p>
  * 示例 1：
  * <p>
@@ -31,11 +34,6 @@ import com.wjd.practice.leetcode.structure.ListNode;
  */
 public class SwapPairs {
 
-    public ListNode swapPairs(ListNode head) {
-        // return iterate(head);
-        return recursive(head);
-    }
-
     /**
      * 思路：哨兵，将链表重新加入新链表，添加的时候，先加第2个，再加第1个
      * <p>
@@ -44,7 +42,9 @@ public class SwapPairs {
      * 执行耗时:0 ms,击败了100.00% 的Java用户
      * 内存消耗:39.2 MB,击败了43.20% 的Java用户
      */
-    private ListNode iterate(ListNode head) {
+    @TestCase(input = {"[1,2,3,4]", "[]", "[1]"},
+            output = {"[2,1,4,3]", "[]", "[1]"})
+    public ListNode iterate(ListNode head) {
         ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
         ListNode first, second, p = head;
@@ -77,7 +77,9 @@ public class SwapPairs {
      * 执行耗时:0 ms,击败了100.00% 的Java用户
      * 内存消耗:39.3 MB,击败了11.94% 的Java用户
      */
-    private ListNode recursive(ListNode head) {
+    @TestCase(input = {"[1,2,3,4]", "[]", "[1]"},
+            output = {"[2,1,4,3]", "[]", "[1]"})
+    public ListNode recursive(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
