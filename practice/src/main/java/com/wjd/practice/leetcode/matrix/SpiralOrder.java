59dba7ec3984cfd70bc467433a9dc3d10330cfcb
weijiaduo <648620282@qq.com>
2023-10-04 16:34:36 +0800

    54. 螺旋矩阵

--- practice/src/main/java/com/wjd/practice/leetcode/matrix/SpiralOrder.java
+++ practice/src/main/java/com/wjd/practice/leetcode/matrix/SpiralOrder.java
@@ -1,5 +1,7 @@
 package com.wjd.practice.leetcode.matrix;
 
+import com.wjd.practice.leetcode.TestCase;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,8 +10,22 @@ import java.util.List;
  * <p>
  * 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
  * <p>
+ * 示例 1：
+ * <p>
  * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
  * 输出：[1,2,3,6,9,8,7,4,5]
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+ * 输出：[1,2,3,4,8,12,11,10,9,5,6,7]
+ * <p>
+ * 提示：
+ * <p>
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 10
+ * -100 <= matrix[i][j] <= 100
  *
  * @since 2022/5/31
  */
@@ -23,6 +39,8 @@ public class SpiralOrder {
      * 执行耗时:0 ms,击败了100.00% 的Java用户
      * 内存消耗:39.4 MB,击败了55.37% 的Java用户
      */
+    @TestCase(input = {"[[1,2,3],[4,5,6],[7,8,9]]", "[[1,2,3,4],[5,6,7,8],[9,10,11,12]]"},
+            output = {"[1,2,3,6,9,8,7,4,5]", "[1,2,3,4,8,12,11,10,9,5,6,7]"})
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> ans = new ArrayList<>();
         int top = -1, bottom = matrix.length;
