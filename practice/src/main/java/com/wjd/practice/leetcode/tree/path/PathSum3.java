d9b19841088e297d323109fe1c5b95af01e59ce9
weijiaduo <648620282@qq.com>
2023-10-11 06:41:35 +0800

    437. 路径总和 III

--- practice/src/main/java/com/wjd/practice/leetcode/tree/path/PathSum3.java
+++ practice/src/main/java/com/wjd/practice/leetcode/tree/path/PathSum3.java
@@ -41,49 +41,43 @@ public class PathSum3 {
      * <p>
      * 复杂度：时间 O(n^2) 空间 O(logn)
      * <p>
-     * 执行耗时:37 ms,击败了10.16% 的Java用户
-     * 内存消耗:41.4 MB,击败了46.85% 的Java用户
+     * 执行耗时:31 ms,击败了40.23% 的Java用户
+     * 内存消耗:41.9 MB,击败了53.66% 的Java用户
      */
     @TestCase(input = {"[10,5,-3,3,2,null,11,3,-2,null,1]", "8",
             "[5,4,8,11,null,13,4,7,2,null,null,5,1]", "22"},
             output = {"3", "3"})
     public int dfs(TreeNode root, int targetSum) {
-        return dfs(root, targetSum, false);
+        if (root == null) {
+            return 0;
+        }
+        int cnt = 0;
+        cnt += rootSum(root, targetSum);
+        cnt += dfs(root.left, targetSum);
+        cnt += dfs(root.right, targetSum);
+        return cnt;
     }
 
     /**
-     * 递归，以每个节点为起始节点，往下遍历所有子节点，寻找目标值
+     * 统计以 root 为起点，满足和为 target 的路径数量
      *
-     * @param root    根节点
-     * @param target  目标值，用 int 类型的话，可能会溢出
-     * @param started 路径是否已经开始
+     * @param root   当前节点
+     * @param target 目标数值
+     * @return 路径数量
      */
-    private int dfs(TreeNode root, long target, boolean started) {
+    private int rootSum(TreeNode root, long target) {
         if (root == null) {
             return 0;
         }
 
-        // 满足条件
         int cnt = 0;
         if (root.val == target) {
-            cnt++;
+            cnt += 1;
         }
-
-        // 从根节点出发
-        cnt += dfs(root.left, target - root.val, true);
-        cnt += dfs(root.right, target - root.val, true);
-
-        // 路径是连续的，如果是从父节点上面下来的路径不能再从子节点出发
-        if (!started) {
-            // 从子节点出发
-            cnt += dfs(root.left, target, false);
-            cnt += dfs(root.right, target, false);
-        }
+        cnt += rootSum(root.left, target - root.val);
+        cnt += rootSum(root.right, target - root.val);
         return cnt;
     }
-
-    // 前缀和频率
-    Map<Long, Integer> map = new HashMap<>();
 
     /**
      * 官方题解
@@ -94,36 +88,37 @@ public class PathSum3 {
      * <p>
      * 复杂度：时间 O(n) 空间 O(logn)
      * <p>
-     * 执行耗时:3 ms,击败了79.34% 的Java用户
-     * 内存消耗:42.3 MB,击败了8.42% 的Java用户
+     * 执行耗时:2 ms,击败了100.00% 的Java用户
+     * 内存消耗:41.18MB,击败了16.35% 的Java用户
      */
     @TestCase(input = {"[10,5,-3,3,2,null,11,3,-2,null,1]", "8",
             "[5,4,8,11,null,13,4,7,2,null,null,5,1]", "22"},
             output = {"3", "3"})
     public int prefix(TreeNode root, int targetSum) {
-        map = new HashMap<>();
-        map.put(0L, 1);
-        return prefix(root, targetSum, 0);
+        // 前缀和频率
+        Map<Long, Integer> freq = new HashMap<>();
+        freq.put(0L, 1);
+        return prefix(root, targetSum, 0, freq);
     }
 
-    private int prefix(TreeNode root, int targetSum, long sum) {
+    private int prefix(TreeNode root, int targetSum, long sum, Map<Long, Integer> freq) {
         if (root == null) {
             return 0;
         }
 
         // 当前节点的前缀和
         sum += root.val;
-        int cnt = map.getOrDefault(sum - targetSum, 0);
+        int cnt = freq.getOrDefault(sum - targetSum, 0);
 
         // 增加新的前缀和
-        map.put(sum, map.getOrDefault(sum, 0) + 1);
+        freq.put(sum, freq.getOrDefault(sum, 0) + 1);
 
         // 递归前缀和
-        cnt += prefix(root.left, targetSum, sum);
-        cnt += prefix(root.right, targetSum, sum);
+        cnt += prefix(root.left, targetSum, sum, freq);
+        cnt += prefix(root.right, targetSum, sum, freq);
 
         // 不连续的前缀和不能用，所以要去掉
-        map.put(sum, map.get(sum) - 1);
+        freq.put(sum, freq.get(sum) - 1);
         return cnt;
     }
 
