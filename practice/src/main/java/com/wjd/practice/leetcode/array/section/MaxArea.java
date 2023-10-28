ebeaf9e39721baeeae6a8d398484ef18e6867eba
weijiaduo <648620282@qq.com>
2023-10-28 20:32:07 +0800

    11. 盛最多水的容器

--- practice/src/main/java/com/wjd/practice/leetcode/array/section/MaxArea.java
+++ practice/src/main/java/com/wjd/practice/leetcode/array/section/MaxArea.java
@@ -45,9 +45,9 @@ public class MaxArea {
      * <p>
      * 容器容量与两边界的短板有关，移动边界时，不论是移动哪一边，容器宽度 r-l 总会减一
      * <p>
-     * 1、若移动短板，容器的高度 Min(h[l], h[r]) 可能会变大或变小，最终容量可能增大或减少
+     * 1、若移动短板，容器的高度 Min(h[l], h[r]) 可能会变大或变小，容量可能增大或减少
      * <p>
-     * 2、若移动长板，容器的高度 Min(h[l], h[r]) 会保持不变或变小，最终容量可能不变或减少
+     * 2、若移动长板，容器的高度 Min(h[l], h[r]) 会保持不变或变小，容量肯定减少
      * <p>
      * 所以，移动长板容量肯定是不会增大的，但是移动短板就有可能增大，因此每次移动都应该移动短板
      * <p>
@@ -62,8 +62,8 @@ public class MaxArea {
     @TestCase(input = {"[1,8,6,2,5,4,8,3,7]", "[1,1]"},
             output = {"49", "1"})
     public int doublePoint(int[] height) {
-        int lp = 0, rp = height.length - 1;
         int maxArea = 0;
+        int lp = 0, rp = height.length - 1;
         while (lp < rp) {
             int area;
             if (height[lp] < height[rp]) {
@@ -76,45 +76,6 @@ public class MaxArea {
             maxArea = Math.max(maxArea, area);
         }
         return maxArea;
-    }
-
-    /**
-     * 思路：动态规划
-     * <p>
-     * 复杂度：时间 O(n^2) 空间 O(n)
-     * <p>
-     * 超出时间限制
-     *
-     * @param height 数组
-     * @return 最大值
-     */
-    @TestCase(input = {"[1,8,6,2,5,4,8,3,7]", "[1,1]"},
-            output = {"49", "1"})
-    public int dynamic(int[] height) {
-        int[] areas = new int[height.length];
-        for (int i = 1; i < height.length; i++) {
-            // 最大值不包括i在内
-            areas[i] = areas[i - 1];
-            // 最大值包括i在内
-            for (int j = 0; j < i; j++) {
-                areas[i] = Math.max(calcArea(height, j, i), areas[i]);
-            }
-        }
-        return areas[height.length - 1];
-    }
-
-    /**
-     * 计算区域面积
-     *
-     * @param height 数组
-     * @param i      索引i
-     * @param j      索引j
-     * @return 区域面积
-     */
-    private int calcArea(int[] height, int i, int j) {
-        int width = Math.abs(i - j);
-        int minHeight = Math.min(height[i], height[j]);
-        return width * minHeight;
     }
 
 }
