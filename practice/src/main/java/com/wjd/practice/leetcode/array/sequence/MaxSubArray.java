bc92d8eccd5f8c705f9a30eacb3fcbe07b4fd433
weijiaduo <648620282@qq.com>
2023-11-15 11:31:04 +0800

    53. 最大子数组和

--- practice/src/main/java/com/wjd/practice/leetcode/array/sequence/MaxSubArray.java
+++ practice/src/main/java/com/wjd/practice/leetcode/array/sequence/MaxSubArray.java
@@ -35,7 +35,38 @@ import com.wjd.practice.TestCase;
 public class MaxSubArray {
 
     /**
-     * 思路：使用累加和来判断子数组要不要连接起来
+     * 思路：动态规划，定义以 i 为结尾的最大子数组和
+     * <p>
+     * 复杂度：时间 O(n) 空间 O(n)
+     * <p>
+     * 执行耗时:2 ms,击败了44.69% 的Java用户
+     * 内存消耗:56.1 MB,击败了66.08% 的Java用户
+     */
+    @TestCase(input = {"[-2,1,-3,4,-1,2,1,-5,4]", "[1]", "[5,4,-1,7,8]"},
+            output = {"6", "1", "23"})
+    public int dynamic1(int[] nums) {
+        // 状态定义
+        // dp[i] 表示以 nums[i] 结尾的子数组的最大和
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        // 状态初始化
+        dp[0] = nums[0];
+
+        // 状态转移
+        // dp[i] = max(dp[i-1] + nums[i], nums[i])
+        int ans = dp[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            ans = Math.max(dp[i], ans);
+        }
+        return ans;
+    }
+
+    /**
+     * 思路：动态规划+滚动数组，优化空间复杂度
+     * <p>
+     * 使用累加和来判断子数组要不要连接起来
      * <p>
      * 累加和 sum[i - 1] > 0 时，sum[i] = sum[i-1] + nums[i]
      * <p>
@@ -50,7 +81,7 @@ public class MaxSubArray {
      */
     @TestCase(input = {"[-2,1,-3,4,-1,2,1,-5,4]", "[1]", "[5,4,-1,7,8]"},
             output = {"6", "1", "23"})
-    public int maxSubArray(int[] nums) {
+    public int dynamic0(int[] nums) {
         int maxSum = nums[0], pre = 0;
         for (int num : nums) {
             pre = Math.max(pre + num, num);
