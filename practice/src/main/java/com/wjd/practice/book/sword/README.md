8b11b8ff6b5e1f552d5b83154d419f9acb6252bc
weijiaduo <648620282@qq.com>
2023-11-22 10:26:59 +0800

    剑指Offer: 3. 数组中重复的数字

---
+++ practice/src/main/java/com/wjd/practice/book/sword/README.md
@@ -0,0 +1,5 @@
+# 剑指 Offer
+
+# 1. 数组
+
+- [3. 数组中重复的数字](array/DuplicateNumber.java)
--- practice/src/main/java/com/wjd/practice/book/sword/array/DuplicateNumber.java
+++ practice/src/main/java/com/wjd/practice/book/sword/array/DuplicateNumber.java
@@ -1,52 +1,54 @@
 package com.wjd.practice.book.sword.array;
 
-import java.util.Arrays;
+import com.wjd.practice.TestCase;
 
 /**
+ * 3. 数组中重复的数字
+ * <p>
  * 在一个长度为n的数组里的所有数字都在0到n-1的范围内。
+ * <p>
  * 数组中某些数字是重复的，但不知道有几个数字是重复的。也不知道每个数字重复几次。
+ * <p>
  * 请找出数组中任意一个重复的数字。
+ * <p>
  * 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2。
+ *
+ * @author weijiaduo
+ * @since 2023/11/22
  */
 public class DuplicateNumber {
 
-    public static void main(String[] args) {
-        int[] numbers = {2, 3, 1, 0};
-        int[] duplication = new int[1];
-        boolean result = duplicate(numbers, numbers.length, duplication);
-        System.out.println(result + " " + duplication[0]);
-    }
-
-    // Parameters:
-    //    numbers:     an array of integers
-    //    length:      the length of array numbers
-    //    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
-    //                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
-    //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
-    // Return value:       true if the input is valid, and there are some duplications in the array number
-    //                     otherwise false
-    public static boolean duplicate(int[] numbers, int length, int[] duplication) {
-        if (numbers == null || numbers.length == 0 || length <= 0) {
-            return false;
+    /**
+     * 思路：遍历数组，将数字放到对应的下标位置，如果下标位置已经有对应的数字，则返回
+     * <p>
+     * 复杂度：时间 O(n) 空间 O(1)
+     */
+    @TestCase(input = {"[2,3,1,0,2,5,3]", "[1,2,0]"},
+            output = {"2", "-1"})
+    public int duplicate(int[] numbers) {
+        if (numbers == null) {
+            return -1;
         }
 
-        int[] num = new int[length];
-        Arrays.fill(num, 0);
-        for (int i = 0; i < length; i++) {
-            int number = numbers[i];
-            if (number < length) {
-                if (num[number] == 0) {
-                    num[number]++;
-                } else {
-                    duplication[0] = number;
-                    return true;
+        int n = numbers.length;
+        for (int i = 0; i < n; i++) {
+            // 摆放数字到对应的下标位置
+            while (numbers[i] != i) {
+                // 超出范围的数字
+                if (numbers[i] >= n) {
+                    return -1;
                 }
-            } else {
-                return false;
+                // 找到重复数字
+                if (numbers[i] == numbers[numbers[i]]) {
+                    return numbers[i];
+                }
+                // 交换数字
+                int temp = numbers[i];
+                numbers[i] = numbers[temp];
+                numbers[temp] = temp;
             }
         }
-
-        return false;
+        return -1;
     }
 
 }
