61d0532c6316ac106217ca236d6fab903377ebc6
weijiaduo <648620282@qq.com>
2023-11-22 11:00:34 +0800

    剑指Offer: 5. 替换空格

--- practice/src/main/java/com/wjd/practice/book/sword/README.md
+++ practice/src/main/java/com/wjd/practice/book/sword/README.md
@@ -4,3 +4,7 @@
 
 - [3. 数组中重复的数字](array/DuplicateNumber.java)
 - [4. 二维数组中的查找](array/FindNumberInSortedArray.java)
+
+# 2. 字符串
+
+- [5. 替换空格](string/ReplaceSpace.java)
--- practice/src/main/java/com/wjd/practice/book/sword/string/ReplaceSpace.java
+++ practice/src/main/java/com/wjd/practice/book/sword/string/ReplaceSpace.java
@@ -1,40 +1,57 @@
 package com.wjd.practice.book.sword.string;
 
+import com.wjd.practice.TestCase;
+
+/**
+ * 5. 替换空格
+ * <p>
+ * 请实现一个函数，把字符串中的每个空格替换成"%20"。
+ * <p>
+ * 例如，输入"We are happy."，则输出"We%20are%20happy."。
+ *
+ * @author weijiaduo
+ * @since 2023/11/22
+ */
 public class ReplaceSpace {
 
-    public static void main(String[] args) {
-        String s = "We Are Happy.";
-        System.out.println(replaceSpace(new StringBuffer(s)));
-    }
-
-    public static String replaceSpace(StringBuffer str) {
-        if (str == null) {
-            return null;
+    /**
+     * 思路：遍历字符串，统计空格数量，然后从后往前替换
+     * <p>
+     * 复杂度：时间 O(n) 空间 O(1)
+     */
+    @TestCase(input = {"We are happy."},
+            output = {"We%20are%20happy."})
+    public String replaceSpace(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
         }
 
-        int count = 0;
-        for (int i = 0; i < str.length(); i++) {
+        // 统计空格数量
+        int n = str.length(), cnt = 0;
+        for (int i = 0; i < n; i++) {
             if (str.charAt(i) == ' ') {
-                count++;
+                cnt++;
             }
         }
-
-        StringBuffer nStr;
-        if (count > 0) {
-            nStr = new StringBuffer();
-            for (int i = str.length() - 1; i >= 0; i--) {
-                if (str.charAt(i) == ' ') {
-                    nStr.append("02%");
-                } else {
-                    nStr.append(str.charAt(i));
-                }
-            }
-            nStr.reverse();
-        } else {
-            nStr = str;
+        // 没有空格
+        if (cnt == 0) {
+            return str;
         }
 
-        return nStr.toString();
+        // 替换空格
+        int k = n + cnt * 2;
+        char[] chars = new char[k];
+        for (int i = n - 1; i >= 0; i--) {
+            char ch = str.charAt(i);
+            if (ch == ' ') {
+                chars[--k] = '0';
+                chars[--k] = '2';
+                chars[--k] = '%';
+            } else {
+                chars[--k] = ch;
+            }
+        }
+        return new String(chars);
     }
 
 }
