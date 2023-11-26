934313701954a8aca4a91bdefdc2c84e3b5c1f84
weijiaduo <648620282@qq.com>
2023-11-26 14:02:13 +0800

    剑指Offer: 36. 二叉搜索树与双向链表

--- practice/src/main/java/com/wjd/practice/book/sword/README.md
+++ practice/src/main/java/com/wjd/practice/book/sword/README.md
@@ -38,6 +38,7 @@
 - [32.3 按之字形顺序打印二叉树](tree/PrintTreeInZigzag.java)
 - [33. 二叉搜索树的后序遍历序列](tree/VerifySequenceOfBST.java)
 - [34. 二叉树中和为某一值的路径](tree/FindPathOfSumInTree.java)
+- [36. 二叉搜索树与双向链表](tree/ConvertTreeToLinkedList.java)
 
 # 5. 栈和队列
 
--- practice/src/main/java/com/wjd/practice/book/sword/tree/ConvertTreeToLinkedList.java
+++ practice/src/main/java/com/wjd/practice/book/sword/tree/ConvertTreeToLinkedList.java
@@ -1,46 +1,75 @@
 package com.wjd.practice.book.sword.tree;
 
+import com.wjd.practice.TestCase;
 import com.wjd.structure.tree.binary.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * 36. 二叉搜索树与双向链表
+ * <p>
  * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。
+ * <p>
  * 要求不能创建任何新的结点，只能调整树中结点指针的指向。
+ *
+ * @author weijiaduo
+ * @since 2023/11/26
  */
 public class ConvertTreeToLinkedList {
 
-    public static void main(String[] args) {
-        String[] s = {"5", "4", "#", "3", "#", "2", "#", "1"};
-        TreeNode tree = TreeNode.build(s);
+    /**
+     * 链表头节点
+     */
+    private TreeNode head = null;
+    /**
+     * 上一个遍历的节点
+     */
+    private TreeNode prev = null;
 
-        TreeNode root = convert(tree);
-        TreeNode p = root;
-        while (p != null) {
-            System.out.print(p.val + " ");
-            p = p.right;
-        }
-    }
-
-    private static TreeNode head = null;
-    private static TreeNode prev = null;
-
-    public static TreeNode convert(TreeNode pRootOfTree) {
-        convertTo(pRootOfTree);
+    /**
+     * 思路：中序遍历的结果就是有序的
+     * <p>
+     * 在中序遍历过程中，将上一个遍历的节点的右指针指向当前节点，当前节点的左指针指向上一个节点
+     * <p>
+     * 从而将二叉搜索树转换成一个排序的双向链表
+     * <p>
+     * 复杂度：时间 O(n) 空间 O(logn)
+     */
+    public TreeNode convert(TreeNode root) {
+        convertTo(root);
         return head;
     }
 
-    public static void convertTo(TreeNode root) {
+    private void convertTo(TreeNode root) {
         if (root == null) {
             return;
         }
 
         convertTo(root.left);
+        // 当前节点的左指针指向上一个节点
         root.left = prev;
         if (prev != null) {
+            // 上一个节点的右指针指向当前节点
             prev.right = root;
         } else {
+            // 第一个节点
             head = root;
         }
         prev = root;
         convertTo(root.right);
     }
+
+    @TestCase(input = "[10,6,14,4,8,12,16]",
+            output = "[4,6,8,10,12,14,16]")
+    public List<Integer> test(TreeNode root) {
+        TreeNode head = convert(root);
+        List<Integer> res = new ArrayList<>();
+        while (head != null) {
+            res.add(head.val);
+            head = head.right;
+        }
+        return res;
+    }
+
 }
