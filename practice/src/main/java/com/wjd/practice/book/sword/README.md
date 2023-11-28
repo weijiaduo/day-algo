4f1e9b88fb0e15f741669a1843181e1889f5a6c1
weijiaduo <648620282@qq.com>
2023-11-28 12:52:56 +0800

    剑指Offer: 50.1 第一个只出现一次的字符

--- practice/src/main/java/com/wjd/practice/book/sword/README.md
+++ practice/src/main/java/com/wjd/practice/book/sword/README.md
@@ -19,6 +19,7 @@
 - [20. 表示数值的字符串](string/NumericStrings.java)
 - [38. 字符串的排列](string/StringPermutation.java)
 - [48. 最长不含重复字符的子字符串](string/LongestSubStringWithoutDup.java)
+- [50. 第一个只出现一次的字符](string/FirstNotRepeatingChar.java)
 
 # 3. 链表
 
--- practice/src/main/java/com/wjd/practice/book/sword/string/FirstNotRepeatingChar.java
+++ practice/src/main/java/com/wjd/practice/book/sword/string/FirstNotRepeatingChar.java
@@ -1,37 +1,52 @@
 package com.wjd.practice.book.sword.string;
 
-import java.util.Arrays;
+import com.wjd.practice.TestCase;
 
 /**
+ * 50.1 第一个只出现一次的字符
+ * <p>
  * 在一个字符串(0<=字符串长度<=10000，
+ * <p>
  * 全部由字母组成)中找到第一个只出现一次的字符,并返回它的位置,
+ * <p>
  * 如果没有则返回 -1（需要区分大小写）.
+ *
+ * @author weijiaduo
+ * @since 2023/11/28
  */
 public class FirstNotRepeatingChar {
 
-    public static void main(String[] args) {
-        String str = "fereffr";
-        System.out.println(firstNotRepeatingChar(str));
-    }
-
-    public static int firstNotRepeatingChar(String str) {
+    /**
+     * 思路：哈希表，
+     * <p>
+     * 第一轮遍历统计每个字符出现的次数
+     * <p>
+     * 第二轮遍历找到第一个只出现一次的字符
+     * <p>
+     * 复杂度：时间 O(n) 空间 O(1)
+     */
+    @TestCase(input = {"fereff"}, output = {"2"})
+    public int hash(String str) {
         if (str == null || "".equals(str)) {
             return -1;
         }
 
+        // 统计每个字符出现的次数
         int[] nums = new int[127];
-        Arrays.fill(nums, 0);
-
-        for (int i = 0; i < str.length(); i++) {
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
             nums[str.charAt(i)]++;
         }
 
-        for (int i = 0; i < str.length(); i++) {
+        // 找到第一个只出现一次的字符
+        for (int i = 0; i < n; i++) {
             if (nums[str.charAt(i)] == 1) {
                 return i;
             }
         }
 
+        // 没有找到
         return -1;
     }
+
 }
