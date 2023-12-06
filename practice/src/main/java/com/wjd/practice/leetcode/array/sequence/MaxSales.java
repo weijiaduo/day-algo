ff24148b997778fe30e3be7101dc2ddbc27fe64f
weijiaduo <648620282@qq.com>
2023-12-06 15:36:45 +0800

    力扣：53. 最大子数组和

--- practice/src/main/java/com/wjd/practice/leetcode/array/sequence/MaxSales.java
+++ practice/src/main/java/com/wjd/practice/leetcode/array/sequence/MaxSales.java
@@ -3,61 +3,58 @@ package com.wjd.practice.leetcode.array.sequence;
 import com.wjd.practice.TestCase;
 
 /**
- * 53. 最大子数组和
+ * LCR 161. 连续天数的最高销售额
  * <p>
- * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 某公司每日销售额记于整数数组 sales，请返回所有 连续 一或多天销售额总和的最大值。
  * <p>
- * 子数组 是数组中的一个连续部分。
+ * 要求实现时间复杂度为 O(n) 的算法。
  * <p>
- * 示例 1：
+ * 示例 1:
  * <p>
- * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输入：sales = [-2,1,-3,4,-1,2,1,-5,4]
  * 输出：6
- * 解释：连续子数组[4,-1,2,1] 的和最大，为6 。
+ * 解释：[4,-1,2,1] 此连续四天的销售总额最高，为 6。
  * <p>
- * 示例 2：
+ * 示例 2:
  * <p>
- * 输入：nums = [1]
- * 输出：1
- * <p>
- * 示例 3：
- * <p>
- * 输入：nums = [5,4,-1,7,8]
+ * 输入：sales = [5,4,-1,7,8]
  * 输出：23
+ * 解释：[5,4,-1,7,8] 此连续五天的销售总额最高，为 23。
  * <p>
  * 提示：
  * <p>
- * 1 <= nums.length <= 10⁵
- * -10⁴ <= nums[i] <= 10⁴
+ * 1 <= arr.length <= 10^5
+ * -100 <= arr[i] <= 100
  *
- * @since 2021-05-29
+ * @author weijiaduo
+ * @since 2023/12/6
  */
-public class MaxSubArray {
+public class MaxSales {
 
     /**
      * 思路：动态规划，定义以 i 为结尾的最大子数组和
      * <p>
      * 复杂度：时间 O(n) 空间 O(n)
      * <p>
-     * 执行耗时:2 ms,击败了44.69% 的Java用户
-     * 内存消耗:56.1 MB,击败了66.08% 的Java用户
+     * 执行耗时:1 ms,击败了49.84% 的Java用户
+     * 内存消耗:49.2 MB,击败了18.99% 的Java用户
      */
     @TestCase(input = {"[-2,1,-3,4,-1,2,1,-5,4]", "[1]", "[5,4,-1,7,8]"},
             output = {"6", "1", "23"})
-    public int dynamic1(int[] nums) {
+    public int dynamic1(int[] sales) {
         // 状态定义
-        // dp[i] 表示以 nums[i] 结尾的子数组的最大和
-        int n = nums.length;
+        // dp[i] 表示以 sales[i] 结尾的子数组的最大和
+        int n = sales.length;
         int[] dp = new int[n];
 
         // 状态初始化
-        dp[0] = nums[0];
+        dp[0] = sales[0];
 
         // 状态转移
-        // dp[i] = max(dp[i-1] + nums[i], nums[i])
+        // dp[i] = max(dp[i-1] + sales[i], sales[i])
         int ans = dp[0];
         for (int i = 1; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            dp[i] = Math.max(dp[i - 1] + sales[i], sales[i]);
             ans = Math.max(dp[i], ans);
         }
         return ans;
@@ -68,22 +65,22 @@ public class MaxSubArray {
      * <p>
      * 使用累加和来判断子数组要不要连接起来
      * <p>
-     * 累加和 sum[i - 1] > 0 时，sum[i] = sum[i-1] + nums[i]
+     * 累加和 sum[i - 1] > 0 时，sum[i] = sum[i-1] + sales[i]
      * <p>
-     * 累加和 sum[i - 1] <= 0时，sum[i] = nums[i]
+     * 累加和 sum[i - 1] <= 0时，sum[i] = sales[i]
      * <p>
      * 由于只用到前一个累加和，所以可以只用一个变量保存上一个累加和即可
      * <p>
      * 复杂度：时间 O(n) 空间 O(1)
      * <p>
-     * 执行耗时:1 ms,击败了100.00% 的Java用户
-     * 内存消耗:56.3 MB,击败了16.69% 的Java用户
+     * 执行耗时:0 ms,击败了100.00% 的Java用户
+     * 内存消耗:49.5 MB,击败了5.79% 的Java用户
      */
     @TestCase(input = {"[-2,1,-3,4,-1,2,1,-5,4]", "[1]", "[5,4,-1,7,8]"},
             output = {"6", "1", "23"})
-    public int dynamic0(int[] nums) {
-        int maxSum = nums[0], pre = 0;
-        for (int num : nums) {
+    public int dynamic0(int[] sales) {
+        int maxSum = sales[0], pre = 0;
+        for (int num : sales) {
             pre = Math.max(pre + num, num);
             maxSum = Math.max(pre, maxSum);
         }
@@ -111,14 +108,14 @@ public class MaxSubArray {
      * <p>
      * 复杂度：时间 O(nlogn) 空间 O(logn)
      * <p>
-     * 执行耗时:8 ms,击败了5.21% 的Java用户
-     * 内存消耗:53.8 MB,击败了98.46% 的Java用户
+     * 执行耗时:2 ms,击败了4.75% 的Java用户
+     * 内存消耗:50.3 MB,击败了5.03% 的Java用户
      */
     @TestCase(input = {"[-2,1,-3,4,-1,2,1,-5,4]", "[1]", "[5,4,-1,7,8]"},
             output = {"6", "1", "23"})
-    public int divide(int[] nums) {
-        int max = nums[0];
-        for (int num : nums) {
+    public int divide(int[] sales) {
+        int max = sales[0];
+        for (int num : sales) {
             max = Math.max(num, max);
         }
         // 如果全是负值，只取一个负数
@@ -127,27 +124,27 @@ public class MaxSubArray {
         }
 
         // 分治法求取最大子数组和
-        return dfs(nums, 0, nums.length - 1)[0];
+        return dfs(sales, 0, sales.length - 1)[0];
     }
 
     /**
      * 分治法
      *
-     * @param nums 数组
-     * @param low  [low, high]
-     * @param high [low, high]
+     * @param sales 数组
+     * @param low   [low, high]
+     * @param high  [low, high]
      * @return {最大子数组和，最大前缀和，最大后缀和，区间和}
      */
-    private int[] dfs(int[] nums, int low, int high) {
+    private int[] dfs(int[] sales, int low, int high) {
         if (low == high) {
-            int val = Math.max(nums[low], 0);
-            return new int[]{val, val, val, nums[low]};
+            int val = Math.max(sales[low], 0);
+            return new int[]{val, val, val, sales[low]};
         }
 
         // 分治法，计算左右区间
         int mid = low + (high - low) / 2;
-        int[] ls = dfs(nums, low, mid);
-        int[] rs = dfs(nums, mid + 1, high);
+        int[] ls = dfs(sales, low, mid);
+        int[] rs = dfs(sales, mid + 1, high);
 
         // 合并分治结果
         int[] values = new int[4];
