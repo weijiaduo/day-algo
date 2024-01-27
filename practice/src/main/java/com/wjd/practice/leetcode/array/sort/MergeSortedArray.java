647b31de6782e3a13176a13fa8ae0e39b44e7785
weijiaduo <648620282@qq.com>
2024-01-27 09:32:11 +0800

    力扣：88. 合并两个有序数组

--- practice/src/main/java/com/wjd/practice/leetcode/array/sort/MergeSortedArray.java
+++ practice/src/main/java/com/wjd/practice/leetcode/array/sort/MergeSortedArray.java
@@ -60,20 +60,14 @@ public class MergeSortedArray {
             "[0]", "0", "[1]", "1"},
             output = {"[1,2,2,3,5,6]", "[1]", "[1]"})
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1, j = n - 1;
-        int k = m + n;
-        while (--k >= 0) {
-            if (j < 0) {
-                break;
-            }
-            if (i < 0) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        for (; k >= 0; k--) {
+            if (i >= 0 && j >= 0) {
+                nums1[k] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+            } else if (j >= 0) {
                 nums1[k] = nums2[j--];
-                continue;
-            }
-            if (nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i--];
             } else {
-                nums1[k] = nums2[j--];
+                break;
             }
         }
     }
