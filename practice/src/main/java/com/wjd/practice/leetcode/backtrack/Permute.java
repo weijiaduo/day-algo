e57aec2ba8af831d642e198b3ab60a862f695ee2
weijiaduo <648620282@qq.com>
2025-04-10 03:53:08 +0800

    力扣：46. 全排列

--- practice/src/main/java/com/wjd/practice/leetcode/backtrack/Permute.java
+++ practice/src/main/java/com/wjd/practice/leetcode/backtrack/Permute.java
@@ -58,9 +58,9 @@ public class Permute {
     /**
      * 回溯
      *
-     * @param nums  候选值
+     * @param nums 候选值
      * @param index 当前索引
-     * @param ans   结果集
+     * @param ans 结果集
      */
     private void backtrack(int[] nums, int index, List<List<Integer>> ans) {
         if (index == nums.length - 1) {
@@ -80,6 +80,55 @@ public class Permute {
             backtrack(nums, index + 1, ans);
             nums[i] = nums[index];
             nums[index] = temp;
+        }
+    }
+
+    /**
+     * 思路：回溯，使用额外空间记录已使用过的数字
+     * 第1个位置，有n种情况
+     * 第2个位置，有n-1种情况
+     * ......
+     * 第n个位置，有1种情况
+     * <p>
+     * 复杂度：时间 O(n!) 空间 O(n)
+     * <p>
+     * 执行耗时:1 ms,击败了96.74% 的Java用户
+     * 内存消耗:43.5 MB,击败了95.93% 的Java用户
+     */
+    @TestCase(input = {"[1,2,3]", "[0,1]", "[1]"},
+            output = {"[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]", "[[0,1],[1,0]]", "[[1]]"})
+    public List<List<Integer>> backtrack2(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> path = new ArrayList<>(nums.length);
+        boolean[] used = new boolean[nums.length];
+        backtrack2(nums, path, used, ans);
+        return ans;
+    }
+
+    /**
+     * 回溯
+     *
+     * @param nums 数组
+     * @param path 当前选中路径
+     * @param used 已选择的数字
+     * @param ans 结果
+     */
+    private void backtrack2(int[] nums, List<Integer> path, boolean[] used, List<List<Integer>> ans) {
+        if (path.size() == nums.length) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+
+            path.add(nums[i]);
+            used[i] = true;
+            backtrack2(nums, path, used, ans);
+            path.remove(path.size() - 1);
+            used[i] = false;
         }
     }
 
